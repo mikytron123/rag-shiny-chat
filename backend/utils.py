f1168f15996acf7d3f66a8701f99449968b03bea
Miky <vyasmiky@gmail.com>
2025-04-04 15:29:02 -0400

    fix(tools): Removed environment code from utils

--- backend/utils.py
+++ backend/utils.py
@@ -1,8 +1,4 @@
 from ollama import Client
-import os
-
-OLLAMA_HOST = os.getenv("OLLAMA_HOST", default="localhost")
-
 
 def get_num_tokens(client: Client, model: str, input: str) -> int:
     embed = client.embed(model=model, input=input)
