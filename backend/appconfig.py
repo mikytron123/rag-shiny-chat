300dcfbf99c8416099f0dcc664d1422a08e5da19
Miky <vyasmiky@gmail.com>
2025-05-05 14:26:44 -0400

    fix(app): added redis env vars

--- backend/appconfig.py
+++ backend/appconfig.py
@@ -1,10 +1,11 @@
 import environ
 
+
 @environ.config(prefix="")
 class AppConfig:
-    ollama_host = environ.var()
+    ollama_host = environ.var(default="localhost")
     ollama_port = environ.var()
-    weaviate_port = environ.var(converter=int)
+    weaviate_port = environ.var(default="localhost", converter=int)
     weaviate_host = environ.var()
     tei_host = environ.var()
     tei_port = environ.var()
@@ -12,5 +13,8 @@ class AppConfig:
     langfuse_port = environ.var()
     langfuse_project_public_key = environ.var()
     langfuse_project_secret_key = environ.var()
+    redis_host = environ.var()
+    redis_port = environ.var()
 
-config = environ.to_config(AppConfig)    
+
+config = environ.to_config(AppConfig)
